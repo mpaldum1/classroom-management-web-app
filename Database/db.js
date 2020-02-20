b064f1a2c8825fc52ff79daafa8502b80a052985
mpaldum1 <mpaldum1@etf.unsa.ba>
2020-02-20 23:11:34 +0100

    WT projekat 2019/2020

--- Database/db.js
+++ Database/db.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("DBWT19","root","root",{host:"127.0.0.1",dialect:"mariadb",logging:false});
+const sequelize = new Sequelize("DBWT19","root","",{host:"127.0.0.1",dialect:"mariadb",logging:false});
 // mysql -> mariadb
 
 const db={};
